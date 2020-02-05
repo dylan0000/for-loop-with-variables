afd5a7077faec959c502269e4979aa8cead8f4d8
dylan0000 <58610746+dylan0000@users.noreply.github.com>
2020-02-05 13:51:16 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,28 @@
+let x = 1
+
+let y = 10
+
+for (let index = x; index <= y; index++) {
+    game.splash("counting to 10: " + index)
+}
+let d = 11
+
+let p = 20
+
+for (let index = d; index <= p; index++) {
+    game.splash("counting to 20: " + index)   
+}
+let a = 5
+
+let b = 0
+
+for (let index = a; index >= b; index--) {
+    game.splash("counting down to 0: " + index)
+}
+let g = 100
+
+let h = 0
+
+for (let index = g; index >= h; index-=10) {
+    game.splash("counting to 0 by 10: " + index)
+}
